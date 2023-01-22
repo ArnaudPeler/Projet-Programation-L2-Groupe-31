d1c86cf01a6265ea6531545d360039d3bff57a63
Arnaud Peler <arnaud.peler@outlook.fr>
2023-01-22 14:51:07 +0100

    revert parce que tout était cassé

--- app.py
+++ app.py
@@ -7,11 +7,12 @@ from flask_bcrypt import Bcrypt
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, PasswordField, TextAreaField, SearchField, SelectField
 from wtforms.validators import InputRequired, Length
-
+from mistune import *
+from markdown import *
 import os
 import mistune
 from markdown import partiuclar_markdown
-
+from files import spawn_files, File
 from dashboard import *
 
 app = Flask(__name__)
@@ -54,7 +55,7 @@ class LoginForm(FlaskForm):
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+    return render_template('index.html')
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -66,7 +67,10 @@ def login():
             if bcrypt.check_password_hash(user.password, form.password.data):
                 login_user(user)
                 session['user'] = user.username
+                # user_folder = root_app_folder/users/user_name
                 session['user_folder'] = os.path.join(os.path.split(__file__)[0], 'users', session['user'])
+                # user_files = spawn_files(markdown_files in user_folder)
+                session['user_files'] = spawn_files([file for file in os.listdir(session['user_folder']) if file.split('.')[1] == 'md'])
                 session['filters'] = []
 
                 return redirect(url_for('dashboard'))
@@ -116,38 +120,22 @@ def contact():
 @app.route('/dashboard/', methods=['GET', 'POST'])
 @login_required
 def dashboard():
-    session['user_files'] = [file for file in os.listdir(session['user_folder']) if file.split('.')[1] == 'md']
-    #session['user_files'] = filter(session['user_files'], session['filters'])
 
     dash_form = DashForm()
-    dash_form.select_tag.choices.extend(get_tags(session['user_folder']))
+    dash_form.select_tag.choices.extend(get_tags(session['user_files']))
     if dash_form.validate_on_submit():
-<<<<<<< HEAD
-=======
-    
-    form = DashForm()
-    form.select_tag.choices.extend(get_tags(user_folder))
-    if form.validate_on_submit():
->>>>>>> Stashed changes
-       match get_submit_type(request.form):
-        case SubmitType.NEW:
-            create_new_question(dash_form.new_question_name, user_folder)
-        case SubmitType.SEARCH:
-=======
         match get_submit_type(request.form):
             case SubmitType.NEW:
-                create_new_question(dash_form.new_name.data)
+                create_file(dash_form.new_name.data)
                 return redirect(url_for('dashboard'))
             case SubmitType.SEARCH:
->>>>>>> master
                 pass
             case SubmitType.AGGREGATE:
-                print(get_selected_questions(request.form))
+                print(get_selected_files(request.form))
             case SubmitType.DELETE:
-                for each in get_selected_questions(request.form):
-                    os.unlink(os.path.join(session['user_folder'], each))
+                print(get_selected_files(request.form))
+                delete_files(get_selected_files(request.form))
                 return redirect(url_for('dashboard'))
-
 
     return render_template('dashboard_bare.html', user=session['user'], files=session['user_files'], form=dash_form)
 
