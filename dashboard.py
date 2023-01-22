7fcbbe705628615d1c6241961eb6afa00477ce99
Arnaud Peler <arnaud.peler@outlook.fr>
2023-01-22 17:12:27 +0100

    refresh_tags() 2 (qui marche j'espère)

--- dashboard.py
+++ dashboard.py
@@ -5,9 +5,10 @@ from flask import request, flash, session
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, SearchField, SelectField
 
-
 from files import File, spawn_file, get_file_from_name
 from markdown import detect_tag
+
+
 def get_tags(files: list[File]) -> list[str]:
     """
 
@@ -16,7 +17,7 @@ def get_tags(files: list[File]) -> list[str]:
     """
     tag_list = []
     for file in files:
-        for tag in file[2] or []: # file[2] = file.tags
+        for tag in file[2] or []:  # file[2] = file.tags
             if tag not in tag_list:
                 tag_list.append(tag)
 
@@ -74,10 +75,12 @@ def create_note(name) -> None:
     else:
         try:
             with open(os.path.join(session['user_folder'], name + '.md'), 'x'):
-                session['user_files'] = session['user_files'] + [spawn_file(name+'.md')] # La session qui casse les couilles
+                session['user_files'] = session['user_files'] + [
+                    spawn_file(name + '.md')]  # La session qui casse les couilles
 
         except FileExistsError:
             flash("A question with this name already exist!")
+
 
 def delete_files(files: list[File]) -> None:
     """
@@ -86,9 +89,10 @@ def delete_files(files: list[File]) -> None:
     :return: None
     """
     for file in files:
-        session['user_files'] = [each for each in session['user_files'] if each != file] # La session qui casse les couilles
+        session['user_files'] = [each for each in session['user_files'] if each != file]  # La session qui casse les couilles
 
         os.unlink(file[1])
+
 
 def get_selected_files(request_form) -> list[File]:
     """
@@ -104,9 +108,23 @@ def get_selected_files(request_form) -> list[File]:
     return [get_file_from_name(file) for file in file_list]
 
 
+def refresh_tags():
+    for each in session['user_files']:
+        with open(each[1], 'r') as file:
+            tags = detect_tag(file.read())
+
+        if set(tags) != set(each[2]):
+            # Enlever each de session['user_files'] :
+            session['user_files'] = [file for file in session['user_file'] if file != each]
+            # Rajouter each actualisé dans session['user_files'] :
+            session['user_files'] = session['user_files'] + [spawn_file(each[0])]
+
+
 class FilterType(Enum):
     TAG = 'Tag'
     Name = 'Name'
+
+
 class Filter(NamedTuple):
     type: FilterType
     value: str
