ca56dc0604900bd0ae3cb2588c4ff43ed46c8a5c
Arnaud Peler <arnaud.peler@outlook.fr>
2023-01-23 01:44:11 +0100

    final commit 4 (on espère 4)

--- setup.sh
+++
@@ -1,4 +0,0 @@
-git clone https://github.com/ArnaudPeler/Projet-Programation-L2-Groupe-31.git
-cd Projet-Programation-L2-Groupe-31
-pip install -r requirements.txt
-echo You can now start the app with 'python3 -m app.py'
